54cfa8380272e505b1971113852ba6dccd1624d3
lil L <57925562+leolopez10@users.noreply.github.com>
2020-01-26 22:25:31 -0600

    going to bed

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.delete("/api/notes/:id", function(request, response) {
 
     for (var i = 0; i < notes.length; i++) {
         if (chosen === notes[i].id) {
-            response.json(notes.splice(notes[i], 1));
+            response.json(notes.splice(chosen, 1));
         }
     }
 });
